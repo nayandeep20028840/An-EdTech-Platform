e73328f27e8d304ccdf15f3835c0ea852218a1b6
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2025-01-16 14:11:17 +0530

    Update cloudinary.js

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,14 +1,22 @@
+// Import the Cloudinary library and access its version 2 API
 const cloudinary = require("cloudinary").v2; //! Cloudinary is being required
 
+// Export a function named 'cloudinaryConnect' to configure Cloudinary
 exports.cloudinaryConnect = () => {
 	try {
+    // Configure Cloudinary with credentials to enable media uploads
 		cloudinary.config({
-			//!    ########   Configuring the Cloudinary to Upload MEDIA ########
+			// ########   Configuring the Cloudinary to Upload MEDIA ########
+      
+      // Cloudinary's cloud name (unique identifier for your Cloudinary account)
 			cloud_name: process.env.CLOUD_NAME,
+      // API key for authentication (provided by Cloudinary for your account)
 			api_key: process.env.API_KEY,
+      // API secret for secure access to your Cloudinary account
 			api_secret: process.env.API_SECRET,
 		});
 	} catch (error) {
+    // Log any error that occurs during the configuration process
 		console.log(error);
 	}
 };
