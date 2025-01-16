1eb4b626febe1558f94bf5cb7377e42293d9ab4f
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2025-01-16 14:21:11 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,10 @@
+// Import the Razorpay module to integrate Razorpay's payment gateway into your application
 const Razorpay = require("razorpay");
 
+// Export an instance of Razorpay to be used for creating and managing payments
 exports.instance = new Razorpay({
+  // The key ID provided by Razorpay to authenticate your application
 	key_id: process.env.RAZORPAY_KEY,
+  // The key secret provided by Razorpay for secure communication
 	key_secret: process.env.RAZORPAY_SECRET,
 });
