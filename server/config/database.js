d33008742b5dfee92f691d6145cde0228546460d
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2025-01-16 14:16:48 +0530

    Update database.js
    commits

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,26 @@
+// Import the Mongoose library to interact with MongoDB
 const mongoose = require("mongoose");
+
+// Load environment variables from a `.env` file into `process.env`
 require("dotenv").config();
 
+// Destructure the MongoDB connection URL from environment variables
 const { MONGODB_URL } = process.env;
 
+// Export a function named 'connect' to establish a connection with MongoDB
 exports.connect = () => {
 	mongoose
 		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
+			useNewUrlparser: true, // Parses MongoDB connection string correctly (legacy option).
+			useUnifiedTopology: true, // Enables the new connection management engine for better performance.
 		})
 		.then(console.log(`DB Connection Success`))
 		.catch((err) => {
+      // Log a failure message for easier debugging
 			console.log(`DB Connection Failed`);
+      // Log the actual error message for detailed information
 			console.log(err);
+      // Exit the Node.js process with a failure code (1) to indicate an error
 			process.exit(1);
 		});
 };
